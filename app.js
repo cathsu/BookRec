76dded3722fe68ce0baf8a8f844e96e5e3ff5427
EC2 Default User <ec2-user@ip-172-31-36-239.us-east-2.compute.internal>
2020-04-15 06:06:41 +0000

    Retrieve book information from API

--- app.js
+++ app.js
@@ -13,8 +13,24 @@ app.get("/", function(req, res){
     res.render("home.ejs");
 });
 
-app.get("/results", function(req, res){
-    res.render("results.ejs");
+// title=A+&author=&subject=Criticism - no image, long title
+// https://www.googleapis.com/books/v1/volumes?q=intitle:Harry%20Potter+inauthor:JK%20Rowling+subject:'' -> no books found
+// https://www.googleapis.com/books/v1/volumes?q=intitle:Harry%20Potter+inauthor:+subject: -> no books found
+// https://www.googleapis.com/books/v1/volumes?q=intitle:Harry%20Potter+inauthor:JK%20Rowling+subject:  -> books found
+
+app.get("/results", async function(req, res){
+    let results = await getResults(req);
+    console.log('in results');
+    // console.log(results.hasOwnProperty("imageLinks"));
+    let count = 0; 
+    results.items.forEach(function(r) {
+        console.log(r.volumeInfo.hasOwnProperty("imageLinks")); 
+        console.log(count);
+        console.log(r.volumeInfo.title);
+        
+        count ++; 
+    })
+    res.render("results.ejs", {results: results});
 });
 
 app.get("/signup", function(req, res){
@@ -32,3 +48,59 @@ app.get("/results/:ISBN", function(req, res){
 app.listen(process.env.PORT || port, function(){
     console.log("server is running...");
 });
+
+
+// empty text input: intitle:+inauthor:+subject:
+function getParameters(req) {
+    let flag1 = 0;
+    let flag2 = 0; 
+    let flag3 = 0;
+    let params = 'q=';
+    if (req.query.title) {
+        params += 'intitle:' + req.query.title
+        flag1 = 1; 
+    } 
+    
+    if (req.query.author) {
+        if (flag1) {
+            params+='+'
+        }
+        params += 'inauthor:' + req.query.author;
+        flag2 = 2; 
+    } 
+    
+    if (req.query.subject) {
+        if (flag1 || flag2) {
+            params+='+'
+        }
+        params += 'subject:' + req.query.subject;
+        flag3 = 1; 
+    } 
+    
+    if (! (flag1 || flag2 || flag3) ) {
+        params += '\'\'';
+    }
+    console.log(params);
+    return params;
+}
+
+function getResults(req) {
+    let URL = 'https://www.googleapis.com/books/v1/volumes?'; 
+    let PARAMS = getParameters(req); 
+    console.log(PARAMS);
+    return new Promise(function(resolve, reject) {
+        request(URL + PARAMS, function(error, response, body) {
+            if (!error && response.statusCode == 200) {
+                let parsedData = JSON.parse(body);
+                console.log("SUCCESS"); 
+                console.log(parsedData);
+                resolve(parsedData);
+            } else {
+                reject(error); 
+                console.log(response.statusCode); 
+                console.log(response.error);
+            }
+            
+        }); //request
+    }); //Promise
+} //getResults
