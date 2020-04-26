ac1ad1fb7871985ace01a2e6a2d05b9151d4858e
EC2 Default User <ec2-user@ip-172-31-80-160.ec2.internal>
2020-04-26 21:07:08 +0000

    removed id field

--- sql/db.sql
+++ sql/db.sql
@@ -43,10 +43,9 @@ INSERT INTO `reviews` (`ISBN`, `username`, `review`, `date`) VALUES
 DROP TABLE IF EXISTS `users`;
 
 CREATE TABLE `users` (
-  `ID` int NOT NULL AUTO_INCREMENT,
   `username` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
   `password` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
-  PRIMARY KEY (`ID`)
+  PRIMARY KEY (`username`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 
